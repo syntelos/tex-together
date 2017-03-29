10ba07801ce10d7f4a0605c281b6428a4da6a31e
John Pritchard <jdp@syntelos.com>
2017-03-29 12:34:24 -0400

    portable

--- import.sh
+++ import.sh
@@ -1,10 +1,62 @@
 #!/bin/bash
 
+overwrite=false
+
 src_pr=journal
 src_fx=tex
 
 tgt_pr=together
 tgt_fx=txt
+
+
+function usage {
+    cat<<EOF>&2
+
+Synopsis
+
+ $0 [-s src-name] [-o] 
+
+Description
+
+ Import today's '*.${src_fx}' from '${src_pr}'.
+
+ Optionally redefine source from '${src_pr}' 
+ to 'src-name'.
+
+ Optionally overwrite target from source.
+
+EOF
+}
+
+while [ -n "${1}" ]
+do
+    case "${1}" in
+
+	-o)
+	    overwrite=true
+	    shift
+	    ;;
+
+	-s)
+	    shift
+	    if [ -n "${1}" ]&&[ -d ../"${1}" ]
+	    then
+		src_pr="${1}"
+		shift
+	    else
+		cat<<EOF>&2
+$0 error in argument source '${1}' is not a directory '../${1}'.
+EOF
+		exit 1
+	    fi
+	    ;;
+
+	*)
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
 
 src_re="../${src_pr}/${src_pr}-$(yyyymmdd)-*.${src_fx}"
 
@@ -15,10 +67,24 @@ then
     do
 	tgt=$(basename ${src} .${src_fx} | sed "s/${src_pr}/${tgt_pr}/").${tgt_fx}
 
-	if [ -f "${tgt}" ]
+	if [ -f "${tgt}" ]&&[ 'false' = "${overwrite}" ]
 	then
 	    echo "X ${tgt}"
-	else
+
+	elif [ "txt" = "${src_fx}" ]&&[ "tex" = "${tgt_fx}" ]
+	then
+	    echo '\input preamble' > ${tgt}
+	    echo >> ${tgt}
+	    cat ${src} >> ${tgt}
+	    echo >> ${tgt}
+	    echo '\bye' >> ${tgt}
+
+	    2>/dev/null git add ${tgt}
+
+	    echo "U ${tgt}"
+
+	elif [ "tex" = "${src_fx}" ]&&[ "txt" = "${tgt_fx}" ]
+	then
 	    egrep -v '^\\(input|bye)' ${src} > ${tgt}
 
 	    2>/dev/null git add ${tgt}
