98c752f9f088eb6a0adccd5322baa16e83e13046
John Pritchard <jdp@syntelos.com>
2021-02-04 11:35:53 -0500

    sync

--- create.sh
+++ create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mk_article=false
+
 wd=$(dirname $0)
 
 #
@@ -9,7 +11,7 @@ function usage {
     cat<<EOF>&2
 Synopsis
 
-  $0 [%component] 
+  $0 [-article|-short] [%component] 
 
 Description
 
@@ -20,6 +22,12 @@ EOF
 for arg in $*
 do
     case ${arg} in
+        -article)
+            mk_article=true
+            ;;
+        -short)
+            mk_article=false
+            ;;
         -h|-?)
         usage
         exit 1
@@ -31,14 +39,30 @@ done
 #
 if file=$(${wd}/next.sh $*) &&[ -n "${file}" ]&&[ ! -f "${file}" ]
 then
-    cat<<EOF>${file}
-\input preamble
+    if ${mk_article}
+    then
+        cat<<EOF>${file}
+\documentclass[12pt,a4paper]{article}
+\begin{document}
+
+\title{Logical existentialism}
+\author{John D.H. Pritchard \thanks{@syntelos, logicalexistentialism@gmail.com}}
+\date{\today}
+\maketitle
+
+\section{}
+
+\end{document}
+EOF
+    else
+        cat<<EOF>${file}
+\input shorts
 
 
 
 \bye
 EOF
-
+    fi
     echo ${file}
 
     git add ${file}
